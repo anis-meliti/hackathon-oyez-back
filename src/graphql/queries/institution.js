5e38111b0a87ba732f90db5faec07f1da0d11e54
wassim-lahbibi <wassim.lahbibi@oyez.fr>
2019-09-22 03:53:31 +0100

    create FindInstitution by name or tags

--- src/graphql/queries/institution.js
+++ src/graphql/queries/institution.js
@@ -1,9 +1,13 @@
 export const institutionQueries = `
-institutions: [Institution]
-institution(id: ID!): Institution
- 
+  institutions: [Institution]
+  institution(id: ID!): Institution
+  FindInstitution(input: String): [Institution]
 `;
 export const institutionResolvers = {
   institutions: (_, $, { models }) => models.Institution.find({}, {}, { sort: { createdAt: -1 } }),
   institution: (_, { id }, { models }) => models.Institution.findById(id),
+  FindInstitution:
+    async (_, { input }, { models }) => models.Institution.find({
+      $or: [{ name: { $regex: `.*${input}.*` } }, { tags: input }],
+    }),
 };
