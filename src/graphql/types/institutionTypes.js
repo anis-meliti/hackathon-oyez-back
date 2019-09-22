b496f2c5172e246c1e9989631d4ac281537fa04b
wassim-lahbibi <wassim.lahbibi@oyez.fr>
2019-09-22 03:23:39 +0100

    fix Institution schema

--- src/graphql/types/institutionTypes.js
+++ src/graphql/types/institutionTypes.js
@@ -2,25 +2,27 @@ const institution = `
 type Institution {
     id: ID!
     name: String
-    login: String
     location: String
     rating: String
     review: String
-    tags: String
+    tags: [String]
     description: String
-    link: String
+    url: String
+    logo: String,
+    phone: String,
     user: User    
 }
 
 input InstitutionInput {
-    login: String
     name: String
     location: String
     rating: String
     review: String
-    tags: String
+    tags: [String]
     description: String
-    link: String
+    url: String
+    logo: String,
+    phone: String,
     user: String
 }
 `;
